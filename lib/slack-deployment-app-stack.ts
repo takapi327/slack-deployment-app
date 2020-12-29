9d5836214aaa585618220a7be09dfa2a48ebfb0e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-29 14:29:32 +0900

    Initial configuration of Lambda

--- lib/slack-deployment-app-stack.ts
+++ lib/slack-deployment-app-stack.ts
@@ -1,9 +1,27 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk    from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class SlackDeploymentAppStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+
+  constructor(
+    scope:  cdk.Construct,
+    id:     string,
+    props?: cdk.StackProps
+  ) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /** Configuration for Lambda */
+    new lambda.Function(this, 'SlackDeploymentHandler', {
+
+      /** Specify the version of Node.js to run */
+      runtime: lambda.Runtime.NODEJS_12_X,
+
+      /** Specify the directory of the Code to be executed */
+      code: lambda.Code.fromAsset('lambda'),
+
+      /** Specify the Lambda function to be executed */
+      handler: 'slack-deployment.handler'
+
+    });
   }
 }
